e67293ee7ba1b83778db97aaffc801a76877d037
Dichan Shrestha <blackuniverse.808@gmail.com>
2024-05-23 18:40:10 +0545

    app-st-code

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,22 @@
+import {resend} from '@/lib/reSend'
+import VerificationEmail from '../../emails/VerificationEmail'
+import { ApiResponse } from '@/types/ApiResponse'
+
+export async function sendVerificationEmail(
+    email: string,
+    username: string,
+    verifyCode: string
+): Promise<ApiResponse> {
+    try {
+        await resend.emails.send({
+            from: 'blackuniverse.808@gmail.com',
+            to: email,
+            subject: 'Mystery message Verification code',
+            react: VerificationEmail({username, otp: verifyCode})
+          });
+        return {success: true, message: "Success sending verfication email"}
+    } catch (emailError) {
+        console.error("error sending verification email")
+        return {success: false, message: "Failed sending verfication email"}
+    }
+}
